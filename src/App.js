f5d91f6f96e5fb7893f90a317f305a8beca5f83f
johnathantng <johnathan.tng@gmail.com>
2019-08-01 18:20:41 -0700

    added logAtLeast5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-import { addUpTo, addUpTo2 } from './components/Functions';
+import { addUpTo, addUpTo2, logAtLeast5 } from './components/Functions';
 
 import './App.css';
 
 function App() {
 //WORKSPACE
-	var t1 = performance.now();
-	addUpTo2(10000000);
-	var t2 = performance.now();
-	console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
+	logAtLeast5(1);
 //WORKSPACE
 	return (
 		<div className="welcome-msg"> Open The Console To See Your Work! </div>
--- src/components/Functions.js
+++ src/components/Functions.js
@@ -9,3 +9,9 @@ export const addUpTo = (n) => {
 export const addUpTo2 = (n) => {
 	return n * (n + 1) / 2;
 }
+
+export const logAtLeast5 = (n) => {
+	for (var i = 1; i <= Math.max(5, n); i++) {
+		console.log(i);
+	}
+}
