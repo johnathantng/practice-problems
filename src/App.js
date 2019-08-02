dc7df4d4822f8618f77343430eb885d680311885
johnathantng <johnathan.tng@gmail.com>
2019-08-01 18:23:54 -0700

    added logAtMost5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { addUpTo, addUpTo2, logAtLeast5 } from './components/Functions';
+import { addUpTo, addUpTo2, logAtLeast5, logAtMost5 } from './components/Functions';
 
 import './App.css';
 
 function App() {
 //WORKSPACE
-	logAtLeast5(1);
+	logAtMost5(3);
 //WORKSPACE
 	return (
 		<div className="welcome-msg"> Open The Console To See Your Work! </div>
--- src/components/Functions.js
+++ src/components/Functions.js
@@ -10,8 +10,14 @@ export const addUpTo2 = (n) => {
 	return n * (n + 1) / 2;
 }
 
-export const logAtLeast5 = (n) => {
+export const logAtLeast5 = (n) => {		//	O(n) will scale proportionally to the value of n
 	for (var i = 1; i <= Math.max(5, n); i++) {
+		console.log(i);
+	}
+}
+
+export const logAtMost5 = (n) => {
+	for (var i = 1; i <= Math.min(5, n); i++) {
 		console.log(i);
 	}
 }
